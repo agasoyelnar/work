e3f2b40a10a1c4abe544d40f9f2482f35af0dd2f
agasoyelnar <Classtime@PC_3_506_8.aztu.edu.az>
2026-03-10 13:10:35 +0400

    text

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿//25
+//Console.Write("eded daxil edin: ");
+//int num =Convert.ToInt32(Console.ReadLine());
+
+//if (num <= 100 && num >= 1) {
+//    Console.WriteLine((num*num*num));
+//}
+//else {
+//    Console.WriteLine("reqem uygun deyil");
+//}
+
+
+//26
+//Console.Write("adinizi axil edin: ");
+//string name = Console.ReadLine();
+//Console.Write("soyadinizi axil edin: ");
+//string surname = Console.ReadLine();
+//Console.WriteLine("welcome " +name+" "+surname);
+
+//27
+//string soz = Console.ReadLine();
+//int uzun = soz.Length;
+//Console.WriteLine(uzun);
+
+
+//28
+//string soz = "alma";
+//int sans = 3;
+//Console.WriteLine("3 sansiniz var!");
+//Console.WriteLine("bu meyvedir");
+//while (sans > 0)
+//{
+//    Console.Write("texminizi daxil edin: ");
+//    string cavab = Console.ReadLine();
+
+//    if (cavab.ToLower() == soz)
+//    {
+//        Console.WriteLine(" sozu tapdiniz.");
+//        break;
+//    }
+//    else
+//    {
+//        sans--;
+//        Console.WriteLine("Sehvdir Qalan sans: " + sans);
+//    }
+//}
+
+//29
+//for (int i = 1; i <= 100; i++)
+//{
+//    if (i % 10 == 7)
+//    {
+//        Console.Write("duz ");
+//    }
+//    else
+//        Console.Write(i + " ");
+//}
